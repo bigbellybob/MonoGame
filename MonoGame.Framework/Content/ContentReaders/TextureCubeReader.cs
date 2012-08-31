6d8cb58aec2e07a481c6cbf8a544542de51b10d6
adrienjeanjean <unknown-user@192.168.178.22>
2012-08-31 14:57:38 +0200

    Added support for compressed TextureCube

--- MonoGame.Framework/Content/ContentReaders/TextureCubeReader.cs
+++ MonoGame.Framework/Content/ContentReaders/TextureCubeReader.cs
@@ -12,7 +12,34 @@ namespace Microsoft.Xna.Framework.Content
 			SurfaceFormat surfaceFormat = (SurfaceFormat)reader.ReadInt32 ();
 			int size = reader.ReadInt32 ();
 			int levels = reader.ReadInt32 ();
-			TextureCube textureCube = new TextureCube(reader.GraphicsDevice, size, levels > 1, surfaceFormat);
+
+            SurfaceFormat convertedFormat = surfaceFormat;
+            switch (surfaceFormat)
+            {
+#if IPHONE
+                // At the moment. If a DXT Texture comes in on iOS, it's really a PVR compressed
+                // texture. We need to use this hack until the content pipeline is implemented.
+                // For now DXT5 means we're using 4bpp PVRCompression and DXT3 means 2bpp. Look at
+                // PvrtcBitmapContent.cs for more information.:
+                case SurfaceFormat.Dxt3:
+                    convertedFormat = SurfaceFormat.RgbaPvrtc2Bpp;
+                    break;
+                case SurfaceFormat.Dxt5:
+                    convertedFormat = SurfaceFormat.RgbaPvrtc4Bpp;
+                    break;
+#elif ANDROID
+                case SurfaceFormat.Dxt1:
+                case SurfaceFormat.Dxt3:
+                case SurfaceFormat.Dxt5:
+                    convertedFormat = SurfaceFormat.Color;
+                    break;
+#endif
+                case SurfaceFormat.NormalizedByte4:
+                    convertedFormat = SurfaceFormat.Color;
+                    break;
+            }
+
+            TextureCube textureCube = new TextureCube(reader.GraphicsDevice, size, levels > 1, convertedFormat);
 			for (int face = 0; face < 6; face++) {
 				for (int i=0; i<levels; i++) {
 					int faceSize = reader.ReadInt32();
