1097d688ad1daee97f7917bceedd526630e403a3
Jeanjean <adrienjeanjean@gmail.com>
2012-09-05 11:21:27 +0200

    Added new MGModelProcessor

---
+++ MonoGame.ContentPipeline/ContentProcessors/Processors/MGModelProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using System.ComponentModel;
+using MonoGameContentProcessors.Content;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+
+namespace MonoGameContentProcessors.Processors
+{
+    [ContentProcessor(DisplayName = "MonoGame Model")]
+    public class MGModelProcessor : ModelProcessor
+    {
+        protected override MaterialContent ConvertMaterial(MaterialContent material, ContentProcessorContext context)
+        {
+            return context.Convert<MaterialContent, MaterialContent>(material, "MGMaterialProcessor");
+        }
+    }
+}
